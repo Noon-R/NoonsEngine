b0c6bed02998c14ed7f7b74c065cfb87a455f9f3
Noon-R <r1234psp@yahoo.co.jp>
2020-05-08 22:07:35 +0900

    Init Window

---
+++ NoonsEngine/main.cpp
@@ -0,0 +1,37 @@
+#include<GLFW/glfw3.h>
+#include<iostream>
+
+int main() {
+
+	if (glfwInit() == GL_FALSE) {
+		return -1;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(640,480, "OpenGL SImple", NULL, NULL);
+
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
+
+	while (glfwWindowShouldClose(window) == GL_FALSE) {
+
+		glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwTerminate();
+
+	return 0;
+
+}
