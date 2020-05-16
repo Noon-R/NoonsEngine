f71003ca7069722d01f02c5e89bb11c03c13a749
Noon-R <r1234psp@yahoo.co.jp>
2020-05-16 20:59:00 +0900

    delete comment

--- NoonsEngine/main.cpp
+++ NoonsEngine/main.cpp
@@ -69,39 +69,6 @@ int main() {
 		glfwPollEvents();
 	}
 
-
-	/*GLFWwindow* window = glfwCreateWindow(640,480, "Noon's Engine", NULL, NULL);
-
-	if (!window) {
-		glfwTerminate();
-		return -1;
-	}
-
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-
-	glfwMakeContextCurrent(window);
-	glfwSwapInterval(1);
-
-	while (glfwWindowShouldClose(window) == GL_FALSE) {
-
-		glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-
-		glBegin(GL_TRIANGLES); {
-			glVertex2f(0, 0.5);
-			glVertex2f(-0.5, -0.5);
-			glVertex2f(0.5, -0.5);
-		}
-		glEnd();
-
-		
-		glfwPollEvents();
-
-	}
-
-	glfwTerminate();*/
 	glfwTerminate();
 	return 0;
 
