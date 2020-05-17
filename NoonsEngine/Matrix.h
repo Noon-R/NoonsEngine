143104ab6a82b68945e20454c18e7c8c27b24e66
Noon-R <r1234psp@yahoo.co.jp>
2020-05-17 23:43:37 +0900

    add identity matrix

--- NoonsEngine/Matrix.h
+++ NoonsEngine/Matrix.h
@@ -11,18 +11,28 @@ public:
 		std::copy(a, a + 16, m_matrix);
 	}
 
-	const GLfloat& operator[](std::size_t i) const {
-		return m_matrix[i];
-	}
-
-	GLfloat &operator[](std::size_t i) {
-		return m_matrix[i];
-	}
-
 	const GLfloat* data() const {
 		return m_matrix;
 	}
 
+	void LoadIdentity() {
+		std::fill(m_matrix, m_matrix + 16, 0.0f);
+		m_matrix[0] = m_matrix[5] = m_matrix[10] = m_matrix[15] = 1.0f;
+	}
+
+	static Matrix identity() {
+		Matrix t;
+		t.LoadIdentity();
+		return t;
+	}
+
+	const GLfloat& operator[](std::size_t i) const {
+		return m_matrix[i];
+	}
+
+	GLfloat& operator[](std::size_t i) {
+		return m_matrix[i];
+	}
 private:
 
 	GLfloat m_matrix[16];
