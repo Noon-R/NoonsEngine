3bdd54c2bad540ad4806f5394ad637eb92bb2299
Noon-R <r1234psp@yahoo.co.jp>
2020-05-18 00:09:29 +0900

    Make Translate Scale Rotate

--- NoonsEngine/Matrix.h
+++ NoonsEngine/Matrix.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <cmath>
 #include <GL/glew.h>
 
 class Matrix {
@@ -23,6 +24,50 @@ public:
 	static Matrix identity() {
 		Matrix t;
 		t.LoadIdentity();
+		return t;
+	}
+
+	static Matrix Translate(GLfloat x, GLfloat y, GLfloat z) {
+		Matrix t;
+		t.LoadIdentity();
+		t[12] = x;
+		t[13] = y;
+		t[14] = z;
+
+		return t;
+	}
+
+	static Matrix Scale(GLfloat x, GLfloat y, GLfloat z) {
+		Matrix t;
+
+		t.LoadIdentity();
+		t[ 0] = x;
+		t[ 5] = y;
+		t[10] = z;
+		return t;
+	}
+
+	static Matrix Rotate(GLfloat a, GLfloat x, GLfloat y, GLfloat z) {
+		Matrix t;
+		const GLfloat d(sqrt(x * x + y * y + z * z));
+
+		if (d > 0.0f) {
+			const GLfloat l(x / d), m(y / d), n(z / d);
+			const GLfloat l2(l * l), m2(m * m), n2(n * n);
+			const GLfloat lm(l * m), mn(m * n), nl(n * l);
+			const GLfloat c(cos(a)), c1(1.0f - c), s(sin(a));
+
+			t.LoadIdentity();
+			t[0] = (1.0f - 12) * c + 12;
+			t[1] = lm * c1 + n * s;
+			t[2] = nl * c1 - m * s;
+			t[4] = lm * c1 + n * s;
+			t[5] = (1.0f - m2) *c + m2;
+			t[6] = mn * c1 + l * s;
+			t[8] = nl * c1 + m * s;
+			t[9] = mn * c1 - l * s;
+			t[10] = (1.0f - n2) * c + n2;
+		}
 		return t;
 	}
 
