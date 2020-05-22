deb90a10c8d6c12ddb902f7ea50d83eb65312044
Noon-R <r1234psp@yahoo.co.jp>
2020-05-22 23:09:57 +0900

    make Sphere

--- NoonsEngine/main.cpp
+++ NoonsEngine/main.cpp
@@ -237,14 +237,15 @@ int main() {
 
 	std::vector<Object::Vertex> solidSphereVertex;
 
+	float pi = acos(-1);
 	for (int j = 0; j <= stacks; ++j) {
 		const float t(static_cast<float>(j) / static_cast<float>(stacks));
-		const float y(cos(acos(-1) * t)), r(sin(acos(-1)+t));
+		const float y(cos(pi * t)), r(sin(pi*t));
 
-		for (int i = 0; i < slices; i++)
+		for (int i = 0; i <= slices; i++)
 		{
 			const float s(static_cast<float>(i) / static_cast<float>(slices));
-			const float z(r * cos(2 * acos(-1) * s)), x(r * sin(2*acos(-1) * s));
+			const float z(r * cos(2 * pi * s)), x(r * sin(2 * pi * s));
 
 			const Object::Vertex v = {x,y,z,x,y,z};
 
@@ -259,7 +260,7 @@ int main() {
 		for (int i = 0; i < slices; ++i) {
 			const GLuint k0(k + i);
 			const GLuint k1(k0 + 1);
-			const GLuint k2(k1 + slices);
+			const GLuint k2(k1 + slices); 
 			const GLuint k3(k2 + 1);
 
 			solidSphereIndex.emplace_back(k0);
