712ed9b33d2bae803e2379b045f33e3f69708e7a
Noon-R <r1234psp@yahoo.co.jp>
2020-05-24 23:33:53 +0900

    expand Vector

--- NoonsEngine/Vector.h
+++ NoonsEngine/Vector.h
@@ -1,7 +1,93 @@
 #pragma once
 #include <array>
-
 #include "Matrix.h"
+
+
+typedef struct Vector3{
+	double x, y, z;
+
+	const Vector3 operator+(const Vector3& value) {
+		Vector3 ret;
+		ret.x = this->x + value.x;
+		ret.y = this->y + value.y;
+		ret.z = this->z + value.z;
+		return ret;
+	}
+
+	const Vector3 operator*(double value) {
+		
+		Vector3 ret = {
+			this->x * value,
+			this->y * value,
+			this->z * value
+		};
+
+		return ret;
+	}
+
+	Vector3& operator+=(const Vector3& value) {
+		
+		Vector3 ret = {
+		this->x += value.x,
+		this->y += value.y,
+		this->z += value.z
+		};
+
+		return ret;
+	}
+
+}Vector3;
+
+typedef struct {
+	double x, y, z, w;
+
+	const Vector4 operator+(const Vector4& value) {
+		Vector4 ret;
+		ret.x = this->x + value.x;
+		ret.y = this->y + value.y;
+		ret.z = this->z + value.z;
+		ret.w = this->w + value.w;
+		return ret;
+	}
+
+	const Vector4 operator*(double value) {
+
+		Vector4 ret = {
+			this->x * value,
+			this->y * value,
+			this->z * value,
+			this->w * value
+		};
+
+		return ret;
+	}
+
+	const Vector4 operator*(const Vector4 value) {
+
+		Vector4 ret = {
+			this->x * value.x,
+			this->y * value.y,
+			this->z * value.z,
+			this->w * value.w
+		};
+
+		return ret;
+	}
+
+	Vector4& operator+=(const Vector4& value) {
+
+		Vector4 ret = {
+		this->x += value.x,
+		this->y += value.y,
+		this->z += value.z,
+		this->w += value.w
+		};
+
+		return ret;
+	}
+
+
+}Vector4;
 
 using Vector = std::array<GLfloat, 4>;
 
