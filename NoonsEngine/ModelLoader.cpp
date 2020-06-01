8a51904ac88d05f746488da1b202abbfb313f1a5
Noon-R <r1234psp@yahoo.co.jp>
2020-06-01 22:48:42 +0900

    add ModelLoader

---
+++ NoonsEngine/ModelLoader.cpp
@@ -0,0 +1,6 @@
+#include "ModelLoader.h"
+
+Object::Vertex* LoadObjFile(char const* name)
+{
+	return nullptr;
+}
---
+++ NoonsEngine/ModelLoader.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+
+#include "Object.h"
+
+Object::Vertex* LoadObjFile(char const* name);
+
