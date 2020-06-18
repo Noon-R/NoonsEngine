02f9bbe0e9023c7acfe6f7f19b3a5755995bfe10
Noon-R <r1234psp@yahoo.co.jp>
2020-06-18 22:03:55 +0900

    ShaderBase Add some Function

--- NoonsEngine/ShaderBase.cpp
+++ NoonsEngine/ShaderBase.cpp
@@ -1,17 +1,8 @@
 #include "ShaderBase.h"
 
-ShaderBase::ShaderBase(WindowBase* window)
-
-: m_window(window)
-, m_program(loadProgram("pointWithNormal.vert", "pointWithNormal.frag"))
-, m_modelviewLoc(glGetUniformLocation(m_program, "modelview"))
-, m_projectionLoc(glGetUniformLocation(m_program, "projection"))
-, m_normalMatrixLoc(glGetUniformLocation(m_program, "normalMatrix"))
-, m_LposLoc(glGetUniformLocation(m_program, "Lpos"))
-, m_LambLoc(glGetUniformLocation(m_program, "Lamb"))
-, m_LdiffLoc(glGetUniformLocation(m_program, "Ldiff"))
-, m_LspecLoc(glGetUniformLocation(m_program, "Lspec"))
-, m_materialLoc(glGetUniformBlockIndex(m_program, "Material"))
+ShaderBase::ShaderBase(WindowBase* window, const char* vert, const char* frag)
+	: m_window(window)
+	, m_program(loadProgram(vert, frag))
 {
 }
 
@@ -24,3 +15,11 @@ int ShaderBase::UseShader()
 	glUseProgram(m_program);
 	return 0;
 }
+
+GLuint ShaderBase::GetUniformLocation(const char* name)
+{
+	return glGetUniformLocation(m_program,name);
+	
+}
+
+
--- NoonsEngine/ShaderBase.h
+++ NoonsEngine/ShaderBase.h
@@ -5,29 +5,21 @@
 
 class ShaderBase {
 public:
-	ShaderBase(WindowBase* window);
+	ShaderBase(WindowBase* window,const char * vert, const char* flag);
 	~ShaderBase();
 
 	int UseShader();
-
+	GLuint GetUniformLocation(const char * name);
+	int SetUniformMatrix3();
+	int SetUniformMatrix4();
+	int SetUniform3f();
+	int SetUniform4f();
 
 private:
 
-	struct ShaderLoc {
-		GLuint location;
-	};
 
 	WindowBase* m_window;
 
 	const GLuint m_program;
-	const GLuint m_modelviewLoc;
-	const GLuint m_projectionLoc;
-	const GLuint m_normalMatrixLoc;
-	const GLuint m_LposLoc;
-	const GLuint m_LambLoc;
-	const GLuint m_LdiffLoc;
-	const GLuint m_LspecLoc;
-
-	const GLuint m_materialLoc;
 
 };
