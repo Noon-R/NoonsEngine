8bde266a732211d93cbb54baef5c88a1467f1835
Noon-R <r1234psp@yahoo.co.jp>
2020-06-23 22:39:27 +0900

    Make NoonType

--- NoonsEngine/NoonBaseTypes.h
+++ NoonsEngine/NoonBaseTypes.h
@@ -1,12 +1,32 @@
 #pragma once
 
-#include <GL/glew.h>
+#ifdef DEBUG_NOONENGINE
 
-#ifndef NOON_TYPES
-#define NOON_TYPES 1
+#ifndef ENGINE_DEBUG
+#define ENGINE_DEBUG 1
+
+typedef float Nfloat;
+typedef int   Nint;
+typedef unsigned int  Nuint;
+
+#endif // !
+
+
+#endif // DEBUG
+
+
+#ifndef NOON_TYPES_OPENGL
+#define NOON_TYPES_OPENGL 1
+
+#include <GL/glew.h>
 
 typedef GLfloat Nfloat;
 typedef GLint   Nint;
 typedef GLuint  Nuint;
+
+#endif
+
+#ifndef NOON_TYPES_DIRECTX
+#define NOON_TYPES_DIRECTX 1
 
 #endif
