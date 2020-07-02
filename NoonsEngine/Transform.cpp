a41299c5341098627f5ecc6e23106524f2ecdaa6
Noon-R <r1234psp@yahoo.co.jp>
2020-07-02 23:11:23 +0900

    set tranfromFunctions

--- NoonsEngine/Transform.cpp
+++ NoonsEngine/Transform.cpp
@@ -6,6 +6,8 @@
 Transform::Transform()
 	: m_pos( )
 	, m_rotation()
+	,m_canPortation(true)
+	,m_isFixing(false)
 {
 }
 
@@ -15,31 +17,56 @@ Transform::~Transform()
 
 int Transform::Translate(float x, float y, float z)
 {
+	if (m_isFixing)return -1;
+	m_pos[0] += x;
+	m_pos[1] += y;
+	m_pos[2] += z;
+
 	return 0;
 }
 
 int Transform::Rotatae(float x, float y, float z, float radian)
 {
+	if (m_isFixing)return -1;
+
+	m_rotation[0] += x;
+	m_rotation[1] += y;
+	m_rotation[2] += z;
+	return 0;
+}
+
+int Transform::FixObject()
+{
+	m_isFixing = true;
 	return 0;
 }
 
 int Transform::SetPosition(float x, float y, float z)
 {
+	if (!m_canPortation)return -1;
+
+	m_pos[0] = x;
+	m_pos[1] = y;
+	m_pos[2] = z;
 	return 0;
 }
 
-int Transform::SetRotation(float x, float y, float z)
+int Transform::SetRotation(float roll, float yaw, float pitch)
 {
+	if (!m_canPortation)return -1;
+	m_rotation[0] = roll;
+	m_rotation[1] = yaw;
+	m_rotation[2] = pitch;
 	return 0;
 }
 
 
 float* Transform::GetPosition()
 {
-	return nullptr;
+	return m_pos;
 }
 
 float* Transform::GetRoattion()
 {
-	return nullptr;
+	return m_rotation;
 }
--- NoonsEngine/Transform.h
+++ NoonsEngine/Transform.h
@@ -6,8 +6,9 @@ public:
 
 	int Translate(float x, float y, float z);
 	int Rotatae(float x, float y, float z, float radian);
+	int FixObject();
 	int SetPosition(float x, float y,float z);
-	int SetRotation(float x, float y, float z);
+	int SetRotation(float roll, float yaw, float pitch);
 	float* GetPosition();
 	float* GetRoattion();
 	
@@ -15,5 +16,7 @@ private:
 	float m_pos[3];
 	float m_rotation[3];
 
+	bool m_canPortation;
+	bool m_isFixing;
 
 };
