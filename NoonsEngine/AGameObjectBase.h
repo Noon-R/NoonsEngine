c80e2f0a458ab9c147cca05c6ee3c9ca26a2dee9
Noon-R <r1234psp@yahoo.co.jp>
2020-07-18 23:52:04 +0900

    0718

--- NoonsEngine/AGameObjectBase.h
+++ NoonsEngine/AGameObjectBase.h
@@ -3,18 +3,19 @@
 #include "Transform.h"
 #include "ShaderBase.h"
 #include "Matrix.h"
+#include "Shape.h"
 
 class AGameObject
 {
 public:
-	AGameObject(AWindowBase* window, const char* vert, const char* frag);
+	AGameObject(AWindowBase* window, Shape * shape, const char* vert, const char* frag);
 	~AGameObject();
-	int DrawObject(float const * Lpos,);
+	int DrawObject(float const * Lpos, float const* Lamb, float const* Ldiff);
 
 private:
 
 	Transform m_transform;
 	ShaderBase m_shader;
-
+	Shape m_shape;
 };
 
