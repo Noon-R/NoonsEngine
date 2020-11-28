3fe4e4e81a928a339daae3c53bd9a6beb6a76086
Noon-R <r1234psp@yahoo.co.jp>
2020-11-28 14:58:50 +0900

    定義場所の移動 Object.h

---
+++ Solution/NoonsEngine/Object.cpp
@@ -0,0 +1,52 @@
+#include "Object.h"
+
+inline Object::Object(AWindowBase* window, GLint size, GLsizei vertexcount, const Noon::GraphicsCore::Vertex* vertex, GLsizei indexcount, const GLuint* index) {
+
+	window->SetWindowContext();
+
+	glGenVertexArrays(1, &m_vao);
+	glBindVertexArray(m_vao);
+
+
+	glGenBuffers(1, &m_vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
+	glBufferData(GL_ARRAY_BUFFER,
+		vertexcount * sizeof(Noon::GraphicsCore::Vertex), vertex, GL_STATIC_DRAW
+	);
+
+	glVertexAttribPointer(0, size, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex* >(0)->position);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex*>(0)->normal);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex*>(0)->color);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex*>(0)->uv);
+	glEnableVertexAttribArray(3);
+
+	glGenBuffers(1, &m_ibo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+		indexcount * sizeof(GLuint), index, GL_STATIC_DRAW
+	);
+
+
+	glBindVertexArray(0);
+
+}
+
+inline Object::~Object() {
+
+	glDeleteVertexArrays(1, &m_vao);
+
+	glDeleteBuffers(1, &m_vbo);
+	glDeleteBuffers(1, &m_ibo);
+
+}
+
+inline void Object::Bind() const {
+	glBindVertexArray(m_vao);
+}
+
+inline void Object::Release() const {
+	glBindVertexArray(0);
+}
--- Solution/NoonsEngine/Object.h
+++ Solution/NoonsEngine/Object.h
@@ -23,54 +23,11 @@ public:
 
 	
 
-	Object(AWindowBase* window, GLint size, GLsizei vertexcount, const Noon::GraphicsCore::Vertex* vertex ,GLsizei indexcount = 0, const GLuint *index = NULL) {
+	Object(AWindowBase* window, GLint size, GLsizei vertexcount, const Noon::GraphicsCore::Vertex* vertex, GLsizei indexcount = 0, const GLuint* index = NULL);
+	virtual ~Object();
 
-		window->SetWindowContext();
+	void Bind() const;
 
-		glGenVertexArrays(1, &m_vao);
-		glBindVertexArray(m_vao);
-
-		
-		glGenBuffers(1, &m_vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, m_vbo);
-		glBufferData(GL_ARRAY_BUFFER,
-			vertexcount * sizeof(Noon::GraphicsCore::Vertex), vertex, GL_STATIC_DRAW
-			);
-
-		glVertexAttribPointer(0, size, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex * >(0) -> position);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex*>(0)->normal);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex*>(0)->color);
-		glEnableVertexAttribArray(2);
-		glVertexAttribPointer(3, 2, GL_FLOAT, GL_FALSE, sizeof(Noon::GraphicsCore::Vertex), static_cast<Noon::GraphicsCore::Vertex*>(0)->uv);
-		glEnableVertexAttribArray(3);
-
-		glGenBuffers(1, &m_ibo);
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ibo);
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-			indexcount * sizeof(GLuint), index, GL_STATIC_DRAW
-		);
-		
-
-		glBindVertexArray(0);
-
-	}
-	virtual ~Object() {
-
-		glDeleteVertexArrays(1, &m_vao);
-
-		glDeleteBuffers(1, &m_vbo);
-		glDeleteBuffers(1, &m_ibo);
-
-	}
-
-	void Bind() const {
-		glBindVertexArray(m_vao);
-	}
-
-	void Release() const {
-		glBindVertexArray(0);
-	}
+	void Release() const;
 };
 
